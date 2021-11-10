c4ccd79d1edf31dc64b7a84deee6053c2c14463f
Ben Groseclose <ben.groseclose01@gmail.com>
2021-11-09 21:13:12 -0500

    Add teams' svgs

---
+++ MyLeague.Football.Core/Models/DefaultFranchise.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLeague.Football.Core.Models
+{
+    /*
+        "Region": "Arizona",
+        "Name": "Cardinals",
+        "Abbrevation": "ARI",
+        "Logo": "arizona-cardinals.svg",
+        "PrimaryColor": "97233F",
+        "SecondaryColor": "000000"
+     */
+    public class DefaultFranchise
+    {
+        public string Region { get; set; }
+        public string Name { get; set; }
+        public string Abbrevation { get; set; }
+        public string Logo { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+    }
+}
---
+++ MyLeague.Football/MyLeague.Football/Models/CreateLeague.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLeague.Football.Models
+{
+    public class CreateLeague : ObservableObject
+    {
+        private string coachFirstName;
+
+        public string CoachFirstName
+        {
+            get => coachFirstName;
+            set => SetProperty(ref coachFirstName, value);
+        }
+
+        private string coachLastName;
+
+        public string CoachLastName
+        {
+            get => coachLastName;   
+            set => SetProperty(ref coachLastName, value);
+        }
+
+        public int franchiseId;
+
+        public int FranchiseId
+        {
+            get => franchiseId;
+            set => SetProperty(ref franchiseId, value);
+        }
+    }
+}
